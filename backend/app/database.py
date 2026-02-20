a453a87e4235022b33cb26c1e804baff60e6571f
DavidGola <david-gola@outlook.fr>
2026-02-20 17:12:59 +0100

    fix test ci

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,16 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
-if ENVIRONMENT == "development":
-    POSTGRES_DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL_DEV")
-else:
-    POSTGRES_DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
 
-if not POSTGRES_DATABASE_URL:
-    raise ValueError("POSTGRES_DATABASE_URL is not set in the environment variables")
+if ENVIRONMENT != "test":
+    if ENVIRONMENT == "development":
+        POSTGRES_DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL_DEV")
+    else:
+        POSTGRES_DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
 
-engine = create_async_engine(POSTGRES_DATABASE_URL, echo=(ENVIRONMENT == "development"))
-async_session = async_sessionmaker(engine, expire_on_commit=False)
+    if not POSTGRES_DATABASE_URL:
+        raise ValueError("POSTGRES_DATABASE_URL is not set in the environment variables")
+
+    engine = create_async_engine(POSTGRES_DATABASE_URL, echo=(ENVIRONMENT == "development"))
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def get_db() -> AsyncSession:
